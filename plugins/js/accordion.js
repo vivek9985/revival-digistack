5ce93b06479ae9c7e38e5f807d4b615da80902cc
vivek9985 <vivekanando363@gmail.com>
2024-02-25 15:36:58 +0600

    designed faq page

---
+++ plugins/js/accordion.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $(".question-one").click(function () {
+    $(".answer-one").slideToggle("slow");
+  });
+  $(".question-two").click(function () {
+    $(".answer-two").slideToggle("slow");
+  });
+  $(".question-three").click(function () {
+    $(".answer-three").slideToggle("slow");
+  });
+  $(".question-four").click(function () {
+    $(".answer-four").slideToggle("slow");
+  });
+  $(".question-five").click(function () {
+    $(".answer-five").slideToggle("slow");
+  });
+  $(".question-six").click(function () {
+    $(".answer-six").slideToggle("slow");
+  });
+  $(".question-seven").click(function () {
+    $(".answer-seven").slideToggle("slow");
+  });
+  $(".question-eight").click(function () {
+    $(".answer-eight").slideToggle("slow");
+  });
+  $(".question-nine").click(function () {
+    $(".answer-nine").slideToggle("slow");
+  });
+  $(".question-ten").click(function () {
+    $(".answer-ten").slideToggle("slow");
+  });
+});
